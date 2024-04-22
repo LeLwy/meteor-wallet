f5a13a42970890531e6011cba33070b4afc1f11f
LeLwy <Louis.Chancioux@gmail.com>
2024-04-22 12:12:36 +0200

    first commit

---
+++ imports/ui/ContactForm.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { ContactsCollection } from '../api/ContactsCollection';
+
+export const ContactForm = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [imageUrl, setImageUrl] = useState('');
+
+    const saveContact = (e) => {
+
+        e.preventDefault();
+        ContactsCollection.insert({ name, email, imageUrl });
+        setName('');
+        setEmail('');
+        setImageUrl('');
+    }
+
+    return(
+        <form>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input 
+                type="text" 
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                id="name" 
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input 
+                type="email" 
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                id="email"
+                />
+            </div>
+            <div>
+                <label htmlFor="imageUrl">Image URL</label>
+                <input 
+                type="text" 
+                onChange={(e) => setImageUrl(e.target.value)}
+                value={imageUrl}
+                id="imageUrl" 
+                />
+            </div>
+            <div>
+                <button type='submit' onClick={saveContact}>Save Contact</button>
+            </div>
+        </form>
+    )
+}
