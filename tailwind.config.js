107dbce99b8c74db253cced9f091f581963b5c0c
LeLwy <Louis.Chancioux@gmail.com>
2024-04-22 16:58:39 +0200

    Ajout du style sur les composants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./imports/ui/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -6,4 +5,3 @@ module.exports = {
   },
   plugins: [],
 }
-
