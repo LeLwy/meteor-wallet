ee593debdc6f42432cb114ffe90fecde088756df
LeLwy <Louis.Chancioux@gmail.com>
2024-04-23 12:53:39 +0200

    Ajout des publications

--- imports/ui/ContactList.jsx
+++ imports/ui/ContactList.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { memo } from "react";
 import {ContactsCollection} from "../api/ContactsCollection";
 import { Meteor } from 'meteor/meteor';
-import {useTracker} from 'meteor/react-meteor-data';
+import {useSubscribe, useFind} from 'meteor/react-meteor-data';
 import { ErrorAlert } from "./components/ErrorAlert";
 import { SuccessAlert } from "./components/SuccessAlert";
 
@@ -10,9 +10,8 @@ export const ContactList = () => {
   const [error, setError] = React.useState("");
   const [success, setSuccess] = React.useState("");
 
-  const contacts = useTracker(() => {
-    return ContactsCollection.find({}, { sort: { createdAt: -1 }}).fetch();
-  });
+  const isLoading = useSubscribe('allContacts');
+  const contacts = useFind(() => ContactsCollection.find({}, {sort: { createdAt: -1 }}));
 
   const showError = ({message}) => {
     setError(message);
@@ -38,7 +37,44 @@ export const ContactList = () => {
         showSuccess({message: "Contact removed !"})
       }
     })
+  };
+
+  if(isLoading()){
+    return (
+      <div>
+        <div className="mt-10">
+          <h3 className="text-xs font-semibold text-gray-500 uppercase tracking-wide">
+            Loading...
+          </h3>
+        </div>
+      </div>
+    )
   }
+
+  const ContactItem = memo(({contact}) => {
+    return(
+      <li className="py-4 flex items-center justify-between space-x-3">
+        <div className="min-w-0 flex-1 flex items-center space-x-3">
+          <div className="flex-shrink-0">
+            <img className="h-10 w-10 rounded-full" src={contact.imageUrl} alt="" />
+          </div>
+          <div className="min-w-0 flex-1">
+            <p className="text-sm font-medium text-gray-900 truncate">{contact.name}</p>
+            <p className="text-sm font-medium text-gray-500 truncate">{contact.email}</p>
+          </div>
+          <div>
+            <a
+              href="#"
+              onClick={(event) => removeContact(event, contact._id)}
+              className="inline-flex items-center shadow-sm px-3 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
+            >
+              Remove
+            </a>
+          </div>
+        </div>
+      </li>
+    )
+  });
 
   return (
     <div>
@@ -49,27 +85,8 @@ export const ContactList = () => {
         {error && <ErrorAlert message={error} />}
         {success && <SuccessAlert message={success} />}
         <ul role="list" className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
-          {contacts.map((person, personIdx) => (
-            <li key={personIdx} className="py-4 flex items-center justify-between space-x-3">
-              <div className="min-w-0 flex-1 flex items-center space-x-3">
-                <div className="flex-shrink-0">
-                  <img className="h-10 w-10 rounded-full" src={person.imageUrl} alt="" />
-                </div>
-                <div className="min-w-0 flex-1">
-                  <p className="text-sm font-medium text-gray-900 truncate">{person.name}</p>
-                  <p className="text-sm font-medium text-gray-500 truncate">{person.email}</p>
-                </div>
-                <div>
-                  <a
-                    href="#"
-                    onClick={(event) => removeContact(event, person._id)}
-                    className="inline-flex items-center shadow-sm px-3 py-0.5 border border-gray-300 text-sm leading-5 font-medium rounded-full text-gray-700 bg-white hover:bg-gray-50"
-                  >
-                    Remove
-                  </a>
-                </div>
-              </div>
-            </li>
+          {contacts.map((contact) => (
+            <ContactItem key={contact._id} contact={contact} />
           ))}
         </ul>
       </div>
