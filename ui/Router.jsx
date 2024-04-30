b98ecdaa40c3cf00427a3d0fac379df7c61683bd
LeLwy <Louis.Chancioux@gmail.com>
2024-04-30 09:57:32 +0200

    Protection des routes

--- ui/Router.jsx
+++ ui/Router.jsx
@@ -6,13 +6,15 @@ import { Access } from './Access.jsx';
 import { ForgotPassword } from './ForgotPassword.jsx';
 import { ResetPassword } from './ResetPassword.jsx';
 import { RoutePaths } from './RoutePaths.jsx';
+import { LoggedUserOnly } from './components/LoggedUserOnly.jsx';
+import { AnonymousOnly } from './components/AnonymousOnly.jsx';
 
 export const Router = () => (
     <Routes>
-        <Route path={RoutePaths.HOME} element={<Home />} />
-        <Route path={RoutePaths.ACCESS} element={<Access />} />
-        <Route path={RoutePaths.FORGOT_PASSWORD} element={<ForgotPassword />} />
-        <Route path={`${RoutePaths.RESET_PASSWORD}/:token`} element={<ResetPassword />} />
+        <Route path={RoutePaths.HOME} element={<LoggedUserOnly><Home /></LoggedUserOnly>} />
+        <Route path={RoutePaths.ACCESS} element={<AnonymousOnly><Access /></AnonymousOnly>} />
+        <Route path={RoutePaths.FORGOT_PASSWORD} element={<AnonymousOnly><ForgotPassword /></AnonymousOnly>} />
+        <Route path={`${RoutePaths.RESET_PASSWORD}/:token`} element={<AnonymousOnly><ResetPassword /></AnonymousOnly>} />
         <Route path="*" element={<NotFound />} />
     </Routes>
 );
